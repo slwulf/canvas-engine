b61109077ffa24dac11b3359aa16a15feb0a9e7f
Austin Wulf <atwulf@gmail.com>
2016-06-23 19:43:43 -0600

    abstract particle emitter, fix initialization, and move testing code into page

--- index.js
+++ index.js
@@ -1,12 +1,111 @@
 (function(document, window) {
 
   const states = {
-    init: 'INIT',
-    start: 'START',
-    play: 'PLAY',
-    pause: 'PAUSE',
-    end: 'END',
+    init: 0,
+    start: 1,
+    play: 2,
+    pause: 3,
+    end: 4,
     last: null
+  }
+
+  // unsorted helper functions
+  // TODO: organize these?
+
+  function randomInRange(max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  function randomInArray(arr) {
+    return arr[randomInRange(arr.length - 1)]
+  }
+
+  /**
+   * Engine
+   *
+   * Initializes the engine and loops.
+   */
+
+  function Engine(settings = {
+      canvas: document.querySelector('canvas'),
+      size: { width: 640, height: 480 }
+  }) {
+    // always act as a constructor
+    if (!(this instanceof Engine)) return new Engine(settings)
+
+    let { canvas, size } = settings
+
+    // handle no canvas found
+    if (!(canvas instanceof HTMLCanvasElement)) {
+      throw new Error(
+        'Engine requires a canvas to render on. Pass a canvas element as `canvas.el` in your settings object. See the documentation for more help.'
+      )
+    }
+
+    this.sizes = {
+      width: size.width,
+      height: size.height
+    }
+
+    canvas.width = this.sizes.width
+    canvas.height = this.sizes.height
+
+    this.context = canvas.getContext('2d')
+
+    this.state = states.init
+  }
+
+  Engine.prototype = {
+    setState (state) {
+      let vals = Object.keys(states).map(k => states[k])
+      let last = states.last
+      states.last = this.state
+
+      if (~vals.indexOf(state)) {
+        this.state = state
+      } else if (states.hasOwnProperty(state)) {
+        this.state = states[state]
+      } else {
+        states.last = last
+        throw new Error('Cannot find state ' + state + '. Possible states: ' + vals.join(', ') + '.')
+      }
+    },
+
+    start ({ loop, render }) {
+      if (this.state === states.init) {
+        this.render = function RENDER_LOOP() {
+          if (this.state === states.pause) return null
+
+          this.context.clearRect(0, 0, this.sizes.width, this.sizes.height)
+
+          render(this.context)
+
+          requestAnimationFrame(RENDER_LOOP.bind(this))
+        }
+
+        function SIM_LOOP() {
+          if (this.state === states.pause) return null
+
+          loop()
+        }
+
+        setInterval(SIM_LOOP.bind(this), 50)
+        requestAnimationFrame(this.render.bind(this))
+
+        this.setState(states.start)
+      }
+    },
+
+    togglePause () {
+      if (this.state === states.pause) {
+        this.setState(states.play)
+        this.render.call(this)
+        console.log('Unpaused')
+      } else {
+        this.setState(states.pause)
+        console.log('Paused')
+      }
+    }
   }
 
   /**
@@ -21,7 +120,7 @@
    *   @key {Function} render Function to handle rendering the object
    */
 
-  function GameObject(settings) {
+  Engine.createObject = function GameObject(settings) {
     if (!(this instanceof GameObject)) return new GameObject(settings)
 
     this.position = { x: 0, y: 0 }
@@ -42,11 +141,12 @@
     }
   }
 
-  GameObject.prototype = {
+  Engine.createObject.prototype = {
     render (ctx) {
       this.methods.render(ctx)
       return this
     },
+
     setPosition ({ x = this.position.x, y = this.position.y }) {
       if (typeof x !== 'number' || typeof y !== 'number') {
         throw new Error('setPosition called with invalid x and y coordinates.')
@@ -55,6 +155,7 @@
       this.position = { x, y }
       return this
     },
+
     setVelocity ({ x = this.velocity.x, y = this.velocity.y }) {
       if (typeof x !== 'number' || typeof y !== 'number') {
         throw new Error('setVelocity called with invalid x and y coordinates.')
@@ -63,6 +164,7 @@
       this.velocity = { x, y }
       return this
     },
+
     setAcceleration (a) {
       if (typeof a !== 'number') throw new Error('setAcceleration must be called with a number.')
       if (a < 0 || a > 1) throw new Error('Acceleration must be between 0 and 1.')
@@ -70,10 +172,12 @@
       this.acceleration = a
       return this
     },
+
     setSize (width, height = width) {
       this.size = { width, height }
       return this
     },
+
     move () {
       let { x: Px, y: Py } = this.position
       let { x: Vx, y: Vy } = this.velocity
@@ -100,164 +204,74 @@
   }
 
   /**
-   * Engine
+   * ParticleEmitter
    *
-   * Initializes the engine and loops.
+   * Emits objects and shit?
    */
 
-  function Engine({
-    canvas = {
-      el: document.querySelector('canvas'),
-      size: { width: 640, height: 480 }
-    }, render = null, loop = null
-  }) {
-    // always act as a constructor
-    if (!(this instanceof Engine)) return new Engine({ canvas, render, loop })
-
-    // handle no canvas found
-    if (canvas.el === null || !canvas.el) {
-      throw new Error(
-        'Engine requires a canvas to render on. Pass a canvas element as `canvas.el` in your settings object. See the documentation for more help.'
-      )
+  Engine.createEmitter = function ParticleEmitter(limit = 25, settings = {}) {
+    if (!(this instanceof ParticleEmitter)) {
+      return new ParticleEmitter(limit, settings)
     }
 
-    // handle missing render and loop fns
-    if (typeof render !== 'function' || typeof loop !== 'function') {
-      throw new Error(
-        'Engine requires a `render` function and a `loop` function. Pass these as properties on your settings object. See the documentation for more help.'
-      )
-    }
+    let { color, position, size, speed, init } = settings
+    let spread = settings.spread || { high: 3, low: -3 }
 
-    this.canvas = {
-      el: canvas.el,
-      size: { width: canvas.size.width, height: canvas.size.height }
-    }
+    this.limit = limit
 
-    this.canvas.el.width = this.canvas.size.width
-    this.canvas.el.height = this.canvas.size.height
+    this.color = settings.color || 'black'
+    this.size = size || 2
 
-    this.context = this.canvas.el.getContext('2d')
+    this.source = { x: position.x || 0, y: position.y || 0 }
+    this.spread = { high: spread.high, low: spread.low }
+    this.speed = typeof speed === 'number' ? Math.abs(speed) : 10
 
-    this.state = states.init
+    this.particles = init ? [this.createParticle(this.source.x, this.source.y)] : []
   }
 
-  Engine.prototype = {
-    setState(state) {
-      if (states.hasOwnProperty(state)) {
-        states.last = this.state
-        this.state = states[state]
+  Engine.createEmitter.prototype = {
+    createParticle (position) {
+      let { size, color, spread, speed, source } = this
+      let { x: srcX, y: srcY } = source
+
+      return Engine.createObject({
+        init: function() {
+          this.setSize(size)
+          this.setPosition({ x: srcX, y: srcY })
+          this.setVelocity({ x: speed, y: randomInRange(spread.high, spread.low) })
+          this.setAcceleration(1)
+
+          this.color = color
+        },
+        render: function(ctx) {
+          let { x, y } = this.position
+          ctx.fillStyle = this.color
+          ctx.beginPath()
+          ctx.arc(x, y, this.size.width, 0, 2 * Math.PI)
+          ctx.fill()
+        }
+      })
+    },
+
+    emit () {
+      this.particles.forEach(function(p) {
+        p.move()
+      })
+
+      if (this.particles.length < this.limit) {
+        this.particles.push(this.createParticle(this.position))
       } else {
-        throw new Error('No state found for ' + state)
+        this.particles.shift()
       }
     },
 
-    start ({ loop, render }) {
-      if (this.state === states.init) {
-        this.render = function RENDER_LOOP() {
-          if (this.state === states.pause) return null
-
-          this.context.clearRect(0, 0, this.canvas.size.width, this.canvas.size.height)
-
-          render(this.context)
-
-          requestAnimationFrame(RENDER_LOOP.bind(this))
-        }
-
-        function SIM_LOOP() {
-          if (this.state === states.pause) return null
-
-          loop()
-        }
-
-        setInterval(SIM_LOOP.bind(this), 50)
-        requestAnimationFrame(this.render.bind(this))
-      }
-    },
-
-    togglePause () {
-      if (this.state === states.pause) {
-        this.state = states.play
-        this.render.call(this)
-      } else {
-        this.state = states.pause
-      }
+    render (ctx) {
+      this.particles.forEach(function(p) {
+        p.render(ctx)
+      })
     }
   }
-
-  Engine.createObject = GameObject
 
   window.Engine = Engine
 
 })(document, window)
-
-
-// API and Testing
-
-function randomInRange(max, min = 0) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-function randomInArray(arr) {
-  return arr[randomInRange(arr.length - 1)]
-}
-
-function ParticleEmitter(limit = 25) {
-  if (!(this instanceof ParticleEmitter)) return new ParticleEmitter(limit)
-
-  this.limit = limit
-  this.particles = [this.createParticle(300, 200)]
-}
-
-ParticleEmitter.prototype.createParticle = function(x = 0, y = 0) {
-  return Engine.createObject({
-    init: function() {
-      this.setSize(3)
-      this.setPosition({ x, y })
-      this.setVelocity({ x: 10, y: randomInRange(3, -3) })
-      this.setAcceleration(1)
-
-      this.color = randomInArray(['red', 'blue', 'orange', 'green'])
-    },
-    render: function(ctx) {
-      let { x, y } = this.position
-      ctx.fillStyle = this.color
-      ctx.beginPath()
-      ctx.arc(x, y, this.size.width, 0, 2 * Math.PI)
-      ctx.fill()
-    }
-  })
-}
-
-ParticleEmitter.prototype.emit = function() {
-  this.particles.forEach(function(p) {
-    p.move()
-  })
-
-  if (this.particles.length < this.limit) {
-    this.particles.push(this.createParticle(300, 200))
-  } else {
-    this.particles.shift()
-  }
-}
-
-ParticleEmitter.prototype.render = function(ctx) {
-  this.particles.forEach(function(p) {
-    p.render(ctx)
-  })
-}
-
-
-let emitter = ParticleEmitter(100)
-
-const engine = Engine({
-  loop: function() {
-    emitter.emit()
-  },
-  render: function(ctx) {
-    emitter.render(ctx)
-  }
-})
-
-engine.canvas.el.onclick = function() {
-  engine.togglePause()
-}
